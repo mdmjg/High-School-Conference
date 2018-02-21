7f880b55f99b3d8c14a33d6313d763cc103c2423
Maria Jaramillo <mariajaramillo@Marias-MacBook-Pro.local>
2018-02-21 20:28:00 +0400

    package json changed, server js changed, config js changed

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'ft7oclyqk6ZG6wFd9lk8XbiGT',
+    consumer_secret: 'CK7wywNoOzzCyX22kBMoGdvZGsVLTnkA5Nb5VR8filZBRfd2uB',
+    access_token_key: '723810421795348480-yyw9YGs3dwcw1dnmq016bf5QQxyTXso',
+    access_token_secret: '1Ak6lrR0svFerM6KAlWpO7gkdIEfuIPE1k0X2DSAd9Y68'
+  }
+}
