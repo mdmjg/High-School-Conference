0fb0d5c675ce12682cb4b90de7e7847b0488a989
Maria Jaramillo <mariajaramillo@Marias-MacBook-Pro.local>
2018-02-24 14:18:51 +0400

    boxes and a message

--- App.js
+++ App.js
@@ -1,34 +1,58 @@
-import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, Image } from 'react-native';
 
-export default class tableView extends Component {
+export default class TwitterUI extends Component {
   render() {
-    const tableHead = ['Time', 'Activity'];
-    const tableTitle = ['10:00', '12:00', '15:00', '17:00'];
-    const tableData = [
-      ['Workshop 1'],
-      ['Workshop 2'],
-      ['Workshop 3'],
-      ['Workshop 4'],
-    ];
     return (
-      <View>
-        <Table>
-          <Row data={tableHead} flexArr={[1, 2]} style={styles.head} textStyle={styles.text}/>
-          <TableWrapper style={{flexDirection: 'row'}}>
-            <Col data={tableTitle} style={styles.title} heightArr={[28,28]} textStyle={styles.text}/>
-            <Rows data={tableData} flexArr={[1, 1]} style={styles.row}/>
-          </TableWrapper>
-        </Table>
+      <View style = {styles.container}>
+        <View style = {styles.tweetBox1}>
+          <Text style = {styles.userNameText}> Hello world! You are amazing! </Text>
+          </View>
+        <View style = {styles.tweetBox2}>
+          <Text style = {styles.userNameText}> My name is Mar </Text>
+          <Image source = {{uri: 'https://facebook.github.io/react/logo-og.png'}}
+                            style = {styles.profilePicture} />
+        </View>
+        <View style = {styles.tweetBox1}>
+          <Text style = {styles.userNameText}> Im so cool </Text>
+        </View>
       </View>
-    )
+    );
   }
 }
-
 const styles = StyleSheet.create({
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  title: { flex: 1, backgroundColor: '#f6f8fa' },
-  row: { height: 28},
-  text: { textAlign: 'center' }
-})
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  userNameText: {
+    fontFamily: 'helvetica',
+    fontSize: 20,
+    color: '#00008b',
+    backgroundColor: 'transparent',
+    textAlign: 'right',
+  },
+  activeTitle: {
+    color: 'red',
+  },
+  tweetBox1: {
+      flex: 9,
+      backgroundColor: '#b0e0e6', //This is a temporary color
+      borderRadius: 20,
+      margin: 10,
+      marginTop: 20,
+    },
+    tweetBox2: {
+        flex: 9,
+        backgroundColor: '#556b2f', //This is a temporary color
+        borderRadius: 20,
+        margin: 10,
+        marginTop: 20,
+      },
+  profilePicture: {
+      width: 44,
+      height: 44,
+      borderRadius: 44/2,
+    },
+});
