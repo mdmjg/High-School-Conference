78514f11aede88d00f17b05f39e8065045d9b6c2
Maria Jaramillo <mariajaramillogv@gmail.com>
2018-02-25 10:07:32 +0400

    second commit

--- App.js
+++ App.js
@@ -6,14 +6,40 @@ export default class TwitterUI extends Component {
     return (
       <View style = {styles.container}>
         <View style = {styles.tweetBox1}>
-          <Text style = {styles.userNameText}> Hello world! You are amazing! </Text>
-          </View>
+         <View style = {styles.imageInTweet}>
+           <Image source = {{uri: 'https://facebook.github.io/react/logo-og.png'}}
+                           style = {styles.profilePicture} />
+         <View style = {styles.textInTweet}>
+           <Text style = {styles.userNameText}> Hello world! You are amazing! </Text>
+        </View>
+        </View>
+      </View>
         <View style = {styles.tweetBox2}>
-          <Text style = {styles.userNameText}> My name is Mar </Text>
+        <View style = {styles.imageInTweet}>
           <Image source = {{uri: 'https://facebook.github.io/react/logo-og.png'}}
-                            style = {styles.profilePicture} />
+                          style = {styles.profilePicture} />
+          <View style = {styles.textInTweet}>
+            <Text style = {styles.userNameText}> My name is Mar </Text>
+            <Text style = {styles.userNameInfo}> Mar </Text>
+          </View>
+          </View>
+          </View>
+        <View style = {styles.tweetBox3}>
+          <Text style = {styles.userNameText}> Im so cool </Text>
         </View>
         <View style = {styles.tweetBox1}>
+          <Text style = {styles.userNameText}> Im so cool </Text>
+        </View>
+        <View style = {styles.tweetBox2}>
+          <Text style = {styles.userNameText}> Im so cool </Text>
+        </View>
+        <View style = {styles.tweetBox3}>
+          <Text style = {styles.userNameText}> Im so cool </Text>
+        </View>
+        <View style = {styles.tweetBox1}>
+          <Text style = {styles.userNameText}> Im so cool </Text>
+        </View>
+        <View style = {styles.tweetBox2}>
           <Text style = {styles.userNameText}> Im so cool </Text>
         </View>
       </View>
@@ -28,8 +54,16 @@ const styles = StyleSheet.create({
   },
   userNameText: {
     fontFamily: 'helvetica',
-    fontSize: 20,
-    color: '#00008b',
+    fontSize: 10,
+    color: 'white',
+    backgroundColor: 'transparent',
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+  userNameInfo: {
+    fontFamily: 'helvetica',
+    fontSize: 10,
+    color: 'gray',
     backgroundColor: 'transparent',
     textAlign: 'right',
   },
@@ -38,21 +72,42 @@ const styles = StyleSheet.create({
   },
   tweetBox1: {
       flex: 9,
-      backgroundColor: '#b0e0e6', //This is a temporary color
+      backgroundColor: 'rgb(181,212,229)', //This is a temporary color
       borderRadius: 20,
       margin: 10,
       marginTop: 20,
     },
+    imageInTweet: {
+      flex: 3,
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    textInTweet: { //For some reason this is being placed in the right, maybe its because it is whatever is to the right
+      flex: 3,
+      flexDirection: 'row',
+      color: 'yellow', //Why isnt the color showing?
+      justifyContent: 'left',
+    },
     tweetBox2: {
         flex: 9,
-        backgroundColor: '#556b2f', //This is a temporary color
+        backgroundColor: 'rgb(158,205,193)', //This is a temporary color
         borderRadius: 20,
         margin: 10,
         marginTop: 20,
+      //  justifyContent: 'center',
       },
-  profilePicture: {
-      width: 44,
-      height: 44,
-      borderRadius: 44/2,
-    },
+      tweetBox3: {
+          flex: 9,
+          backgroundColor: 'rgb(93,129,177)', //This is a temporary color
+          borderRadius: 20,
+          margin: 10,
+          marginTop: 20,
+        //  justifyContent: 'center',
+        },
+      profilePicture: {
+          width: 55,
+          height: 55,
+          borderRadius: 55/2,
+          alignItems: 'center', //How do I make the image show in the center
+        },
 });
